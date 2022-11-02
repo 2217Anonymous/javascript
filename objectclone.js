d73ee81fca9ba1e57f16c47bf1a2834211193bd6
Venkat <venkatanonymous248@gmail.com>
2022-11-02 20:14:53 +0530

    Find Method

--- objectclone.js
+++ objectclone.js
@@ -1,30 +1,47 @@
-let user1 = {
-    name        : "Venkat",
-    age         : 26,
-    intrest     : ["Cricket","Computer"],
+// let user1 = {
+//     name        : "Venkat",
+//     age         : 26,
+//     intrest     : ["Cricket","Computer"],
     
-    greetings : function(){
-        let msg = `My Name is ${this.name},i love ${this.intrest[0]} & ${this.intrest[1]}`
-        console.log(msg);
-    }
-};
+//     greetings : function(){
+//         let msg = `My Name is ${this.name},i love ${this.intrest[0]} & ${this.intrest[1]}`
+//         console.log(msg);
+//     }
+// };
 
-let userDetails={
-    city    : 'Tiruppur',
-    state   : 'TN',
-}
-
-// for(let dt in userDetails){
-//     user1[dt] = userDetails[dt]
+// let userDetails={
+//     city    : 'Tiruppur',
+//     state   : 'TN',
 // }
 
-// console.log(user1);
-// console.log(userDetails);
+// // for(let dt in userDetails){
+// //     user1[dt] = userDetails[dt]
+// // }
 
-user1 = Object.assign({},userDetails)
-console.log(userDetails);
+// // console.log(user1);
+// // console.log(userDetails);
+
+// user1 = Object.assign({},userDetails)
+// console.log(userDetails);
 
 // user1 = {
 //     ...userDetails
 // }
 // console.log(user1);
+
+const userDt = [
+    {
+        id:1,
+        name:"Apple"
+    },
+    {
+        id:1,
+        name:"Banana"
+    },
+]
+
+let res = userDt.find(function(userDt){
+    return userDt.name = "Banana";
+})
+
+console.log(res);
