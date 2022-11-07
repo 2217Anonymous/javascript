6715348d17435d2f3e84587479dd08c334ed72d3
Venkat <venkatanonymous248@gmail.com>
2022-11-07 18:15:33 +0530

    Objects

--- objects.js
+++ objects.js
@@ -1,20 +1,21 @@
-// let User = {
-//     name : "VENKAT",
-//     age : 26,
-//     Sports : ['cricket','football'],
-//     family : {
-//         mother : 'mythily',
-//         father : 'Tiruppathi',
-//         brother : 'Surya',
-//         mine    : this.name,   
-//     },
-//     welcome : function(){
-//         let Welcome = `Welcome Our Home Mr.${this.name} and your family ` 
-//         console.log(Welcome);
-//     }
-// }
+//Objects
+let User = {
+    name : "VENKAT",
+    age : 26,
+    Sports : ['cricket','football'],
+    family : {
+        mother : 'mythily',
+        father : 'Tiruppathi',
+        brother : 'Surya',
+        mine    : this.name,   
+    },
+    welcome : function(){
+        let Welcome = `Welcome Our Home Mr.${this.name} and your family ` 
+        console.log(Welcome);
+    }
+}
 
-//User.welcome();
+User.welcome();
 
 //Factory Function
 
