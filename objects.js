ab358ff8db64a6448fe4e2472d69077370c98b48
Venkat <venkatanonymous248@gmail.com>
2022-11-08 20:51:42 +0530

    function are objects

--- objects.js
+++ objects.js
@@ -39,8 +39,23 @@ function createUser(name){
 
 let Username = createUser("Venkat")
 
-console.log(Username.name);
+Username.name;
 
-console.log(createUser("Anonymous"));
+createUser("Anonymous");
 
-console.log(createUser("Sakthi").welcome());
+createUser("Sakthi").welcome();
+
+//Constructor Function
+
+function CreatePerson(username){
+    let name={
+        name:"Anonymous",
+    } 
+    this.username = username;
+    this.greeting = function(){
+        console.log(`This is ${name.name}`);
+    }
+}
+
+let Person = new CreatePerson("Venkat");
+Person.greeting();
