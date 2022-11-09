542990366689be960baedc61289f14f3fec6457d
Venkat <venkatanonymous248@gmail.com>
2022-11-09 18:11:59 +0530

    factory function

--- objects.js
+++ objects.js
@@ -1,17 +1,43 @@
-let User = {
-    name : "VENKAT",
-    age : 26,
-    Sports : ['cricket','football'],
-    family : {
-        mother : 'mythily',
-        father : 'Tiruppathi',
-        brother : 'Surya',
-        mine    : this.name,   
-    },
-    welcome : function(){
-        let Welcome = `Welcome Our Home Mr.${this.name} and your family ` 
-        console.log(Welcome);
+// let User = {
+//     name : "VENKAT",
+//     age : 26,
+//     Sports : ['cricket','football'],
+//     family : {
+//         mother : 'mythily',
+//         father : 'Tiruppathi',
+//         brother : 'Surya',
+//         mine    : this.name,   
+//     },
+//     welcome : function(){
+//         let Welcome = `Welcome Our Home Mr.${this.name} and your family ` 
+//         console.log(Welcome);
+//     }
+// }
+
+//User.welcome();
+
+//Factory Function
+
+function createUser(name){
+    return {
+        name,
+        age : 26,
+        Sports : ['cricket','football'],
+        family : {
+            mother : 'mythily',
+            father : 'Tiruppathi',
+            brother : 'Surya',
+            mine    : this.name,   
+        },
+        welcome(){
+            let Welcome = `Welcome Our Home Mr.${this.name} and your family ` 
+            console.log(Welcome);
+        }
     }
 }
 
-User.welcome();
+let Username = createUser("Venkat")
+
+console.log(Username.name);
+
+console.log(createUser("Sakthi").welcome());
