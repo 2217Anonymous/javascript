f16e4f3688f606cf1754742932e0f8d5562f9f83
Venkat <venkatanonymous248@gmail.com>
2022-11-13 15:20:39 +0530

    Export

--- primitive.js
+++ primitive.js
@@ -1,11 +1,19 @@
-let a = {
-    value:10
-};
+// let a = {
+//     value:10
+// };
 
-function demo(data){
-    let b = data.value++
+// function demo(data){
+//     let b = data.value++
     
-}
+// }
 
-let c = demo(a)
-console.log("data",a);
+// let c = demo(a)
+// console.log("data",a);
+const Old_Cart      = ["Cricket Bat","Ball","Football"]
+const New_Cart      = ["Helmet","Gloves"]
+const Shopping = [{
+    iteam : "Tennis Ball",
+}]
+let Order = Old_Cart.concat(New_Cart,...Shopping)
+console.log(Order);
+
