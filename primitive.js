f4ab7c3e2d0ff9e3af593cfd4fb77aa4d3f27e19
Venkat <venkatanonymous248@gmail.com>
2022-11-16 10:23:59 +0530

    promise

--- primitive.js
+++ primitive.js
@@ -19,3 +19,16 @@ let Order = Old_Cart.concat(New_Cart,...Shopping)
 console.log(Order);
 
 Order.forEach((data,dataIndex) => console.log(data))
+
+const Old_Object = [
+    {id:1,name:"Cricket Bat"},
+    {id:2,name:"Ball"},
+    {id:3,name:"Football"},
+]
+Old_Object.sort(function(a,b){
+    if(a.name < b.name) return -1; 
+    if(a.name > b.name) return 1; 
+    if(a.name === b.name) return 0; 
+});
+
+console.log(Old_Object);
