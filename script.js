94baf6e25633475db612dd74eaeb938df3176186
Venkat <venkatanonymous248@gmail.com>
2022-11-22 16:06:10 +0530

    for

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-let user = {
+const user = {
     uname:"Anonymous",
     greeting(){
         console.log("Hi Welcome to our Home" + " " + this.uname);
     }
 }
-
 let arrayuser = ["venkat",user,24]
 
-for(let dt in user){
-    console.log(user[dt]);
+
+// user.fname = "Venkat";
+// user.lname = "Anonymous";
+
+// user.fullname=function (){
+//     console.log(`${this.fname} ${this.lname}`);
+// }
+
+// delete user.fullname()
+
+// for(let dt of Object.keys(user)){
+//     console.log(dt);
+// }
+
+for(let dt of Object.entries(user)){
+    console.log(dt[1]);
 }
 
-for(let dt in arrayuser) {
-    console.log(arrayuser[dt]);
-}
+// for(let dt in user){
+//     console.log((user[dt]));
+// }
 
-for(let dt of arrayuser){
-    console.log(dt);
-}
+// for(let dt in arrayuser) {
+//     console.log(arrayuser[dt]);
+// }
+
+// for(let dt of arrayuser){
+//     console.log(dt);
+// }
